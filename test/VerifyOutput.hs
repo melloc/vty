30f21a79bba24013d7e6f49bcf5923588eeeac13
Corey O'Connor <coreyoconnor@gmail.com>
2014-05-02 12:07:06 -0700

    first pass at making a minimal test for Cabal issue #1810

--- test/VerifyOutput.hs
+++ test/VerifyOutput.hs
@@ -5,14 +5,10 @@
 module VerifyOutput where
 import Verify
 
-import Graphics.Vty
-
-import Verify.Graphics.Vty.Image
-import Verify.Graphics.Vty.Output
+import Verify.Graphics.Vty.Output (terminalsOfInterest)
 
 import Control.Monad
 
-import qualified System.Console.Terminfo as Terminfo
 import System.Posix.Env
 import System.IO
 
@@ -20,10 +16,7 @@ tests :: IO [Test]
 tests = concat <$> forM terminalsOfInterest (\termName -> do
     -- check if that terminfo exists
     putStrLn $ "testing end to end for terminal: " ++ termName
-    mti <- try $ Terminfo.setupTerm termName
-    case mti of
-        Left (_ :: SomeException) -> return []
-        Right _ -> return [ verify ("verify " ++ termName ++ " could output a picture")
+    return [ verify ("verify " ++ termName ++ " could output a picture")
                                    (smokeTestTermNonMac termName)
                           -- this is excessive.
                           , verify ("verify " ++ termName ++ " could output a picture on a Mac.")
@@ -31,33 +24,19 @@ tests = concat <$> forM terminalsOfInterest (\termName -> do
                           ]
     )
 
-smokeTestTermNonMac :: String -> Image -> Property
+smokeTestTermNonMac :: String -> () -> Property
 smokeTestTermNonMac termName i = liftIOResult $ do
     -- unset the TERM_PROGRAM environment variable if set.
     -- Required to execute regression test for #42 on a mac
     unsetEnv "TERM_PROGRAM"
     smokeTestTerm termName i
 
-smokeTestTermMac :: String -> Image -> Property
+smokeTestTermMac :: String -> () -> Property
 smokeTestTermMac termName i = liftIOResult $ do
     setEnv "TERM_PROGRAM" "Apple_Terminal" True
     smokeTestTerm termName i
 
-smokeTestTerm :: String -> Image -> IO Result
-smokeTestTerm termName i = do
-    nullOut <- openFile "/dev/null" WriteMode
-    t <- outputForNameAndIO termName nullOut
-    putStrLn $ "context color count: " ++ show (contextColorCount t)
-    reserveDisplay t
-    dc <- displayContext t (100,100)
-    -- always show the cursor to produce tests for terminals with no cursor support.
-    let pic = (picForImage i) { picCursor = Cursor 0 0 }
-    outputPicture dc pic
-    setCursorPos t 0 0
-    when (supportsCursorVisibility t) $ do
-        hideCursor t
-        showCursor t
-    releaseDisplay t
-    releaseTerminal t
+smokeTestTerm :: String -> () -> IO Result
+smokeTestTerm termName () = do
     return succeeded
 
